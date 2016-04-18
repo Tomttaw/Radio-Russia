c3c88414d0aada046963d7ca5672ea17eb49f598
ShannonBakker <shannonc2m@live.nl>
2016-04-18 21:07:03 +0200

    code

--- ukrainecode.py
+++ ukrainecode.py
@@ -7,8 +7,34 @@ This is a temporary script file.
 
 
 import csv
+
+class Province(object):
+    """
+    A Position represents a location in a two-dimensional room.
+    """
+
+    def __init__(self, province_number, adjacent):
+        """
+        Initializes a position with coordinates (x, y).
+        """
+        self.province_number = province_number
+        self.adjacent = adjacent
+        
+
+provinces = []
+counter = 0
 with open('oekraine_provincies.csv', 'rb') as csvfile:
-    ukrainereader = csv.reader(csvfile, delimiter = ',')
+    ukrainereader = csv.reader(csvfile, delimiter = ';')
     for row in ukrainereader:
-        print ("provincie %i", row[0][0])  
+        adjacent = []
+        for i in range
+        print ("provincie %i", row[0])
+        provinces.append(Province(row[0]))
+        print provinces[counter].province_number
+        
+        counter +=1
+        #Province = Province(row[0]) 
+        #Province.province_number = row[0]
+        #print Province.province_number
+       
         
