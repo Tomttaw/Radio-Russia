cc91aa31cefb8147057b27ca315d8ab3e4205848
ShannonBakker <shannonc2m@live.nl>
2016-04-19 19:30:22 +0200

    Werkende code

--- ukrainecode.py
+++ ukrainecode.py
@@ -7,34 +7,45 @@ This is a temporary script file.
 
 
 import csv
+import random
 
 class Province(object):
     """
-    A Position represents a location in a two-dimensional room.
+    A Province represents a province of Ukraine
     """
 
     def __init__(self, province_number, adjacent):
         """
-        Initializes a position with coordinates (x, y).
+        Initializes a province with a random zender-type
         """
         self.province_number = province_number
         self.adjacent = adjacent
+        self.sender_type = random.randrange(0,4)
         
 
 provinces = []
 counter = 0
-with open('oekraine_provincies.csv', 'rb') as csvfile:
+with open('oekraine_provincies2.csv', 'rb') as csvfile:
     ukrainereader = csv.reader(csvfile, delimiter = ';')
     for row in ukrainereader:
+        #print row
         adjacent = []
-        for i in range
-        print ("provincie %i", row[0])
-        provinces.append(Province(row[0]))
-        print provinces[counter].province_number
-        
+        for i in range(len(row)-1):
+            i+=1
+            if (row[i]):
+                adjacent.append(int(row[i]))
+        provinces.append(Province(int(row[0]),adjacent))
         counter +=1
-        #Province = Province(row[0]) 
-        #Province.province_number = row[0]
-        #print Province.province_number
+
+problem = 0
+# check the adjacent sender types and alert if there is a problem
+for province in provinces:
+    print province.province_number, " borders ", province.adjacent
+    for province_adjacent in province.adjacent:
+        if (provinces[province_adjacent].sender_type == province.sender_type):
+            print "probleem", province.province_number, " and ", provinces[province_adjacent].province_number, "have the same sender type"
+            problem+=1
+
+print "aantal problemen:", problem
        
         
