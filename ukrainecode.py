258c8da108903dea702750ba4d57a05ff87aeb6e
Tomttaw <tom-s@live.nl>
2016-04-26 18:51:34 +0200

    Werkend algoritme, nieuw csv
    Algoritme is alleen nog "een" oplossing, nog niet gebaseerd op een
    gelijke verdeling van zenders.
    nieuw csv file is gebaseerd op prioriteit bij:
    Oneven aantal provincies dat de provincie omringd:
    daarna aantal provincies dat aangrenzen
    Bijbehorend kaartje bij csv file

--- ukrainecode.py
+++ ukrainecode.py
@@ -25,7 +25,7 @@ class Province(object):
 
 provinces = []
 counter = 0
-with open('oekraine_provincies3.csv', 'rb') as csvfile:
+with open('oekraine_priority.csv', 'rb') as csvfile:
     ukrainereader = csv.reader(csvfile, delimiter = ';')
     for row in ukrainereader:
         #print row
@@ -37,22 +37,25 @@ with open('oekraine_provincies3.csv', 'rb') as csvfile:
         provinces.append(Province(int(row[0]),adjacent))
         counter +=1        
 
-sender_list = ["A", "B", "C", "D", "E", "F", "G"] 
-
-for province in provinces:
+for provi in provinces:
+    #print provi.province_number, "borders", provi.adjacent
     possible_list = ["A", "B", "C", "D", "E", "F", "G"]
-    for province_adjacent in province.adjacent:
-        if(provinces[province_adjacent].sender_type in possible_list):
-            print provinces[province_adjacent].province_number, "can't have", provinces[province_adjacent].sender_type 
+    for province_adjacent in provi.adjacent:
+        #print provinces[province_adjacent].province_number, provinces[province_adjacent].sender_type
+        if provinces[province_adjacent].sender_type in possible_list:
             possible_list.remove(provinces[province_adjacent].sender_type)
-            print possible_list
-        province.sender_type = possible_list[0]        
+            #print possible_list
+    provi.sender_type = possible_list[0]
+    print provi.province_number, provi.sender_type
+    #print "\n"        
+        
+    
         
         
 problem = 0
 # check the adjacent sender types and alert if there is a problem
 for province in provinces:
-    print province.province_number, " borders", province.borders,"Provinces:", province.adjacent, "has sender type", province.sender_type
+    #print province.province_number, " borders", province.borders,"Provinces:", province.adjacent, "has sender type", province.sender_type
     for province_adjacent in province.adjacent:
         if (provinces[province_adjacent].sender_type == province.sender_type):
             print "problem:", province.province_number, " and ", provinces[province_adjacent].province_number, "have the same sender type"
