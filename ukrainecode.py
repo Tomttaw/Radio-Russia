d7e5e613a01c15c528a849641f24b12b94f9a9fa
Tomttaw <tom-s@live.nl>
2016-05-09 12:58:54 +0200

    Functies, comments

--- ukrainecode.py
+++ ukrainecode.py
@@ -8,6 +8,7 @@ This is a temporary script file.
 
 import csv
 import random
+import sys
 from collections import Counter
 
 class Province(object):
@@ -46,6 +47,34 @@ volgorde = [7,5,6,4,3,2,1]
 sender_list = []
 sender_count = {"A": 0, "B": 0, "C": 0, "D": 0}
 
+"""
+Get list of senders possible for province
+"""
+
+def getpossible(senderlist, provincelist):
+    
+    # iterate adjacent provinces and return list of possible sendertypes
+    for adj_province in provincelist:
+        if provinces[adj_province].sender_type in senderlist:
+            senderlist.remove(provinces[adj_province].sender_type)
+    if not senderlist:
+        print "No sendertype possible"
+        sys.exit()        
+    return senderlist        
+
+# check the adjacent sender types and alert if there is a problem
+def check():
+    problem = 0    
+    for province in provinces:
+        #print province.province_number, " borders", province.borders,"Provinces:", province.adjacent, "has sender type", province.sender_type
+        for province_adjacent in province.adjacent:
+            if (provinces[province_adjacent].sender_type == province.sender_type):
+                print "problem:", province.province_number, " and ", provinces[province_adjacent].province_number, "have the same sender type"
+                problem+=1
+    
+    print "Problems:", problem
+
+
 for i in volgorde:
     same_borders = []
     for provi in provinces:
@@ -59,34 +88,22 @@ for i in volgorde:
             same_borders.remove(random_province)
             #print "random", random_province.province_number
             
-            possible_list = ["A", "B", "C", "D"]
+            sender_lis = ["A", "B", "C", "D"]
             
-            for province_adjacent in random_province.adjacent:
-                #print provinces[province_adjacent].province_number, provinces[province_adjacent].sender_type
-                if provinces[province_adjacent].sender_type in possible_list:
-                    possible_list.remove(provinces[province_adjacent].sender_type)
-            #if not possible_list:
-                #return False
+            possible_list = getpossible(sender_lis, random_province.adjacent)
+            # update possible dictionary and pick least present sender type
             possible_dict = dict((k, sender_count[k]) for k in possible_list)
             random_province.sender_type = min(possible_dict, key=possible_dict.get)
             sender_list.append(random_province.sender_type)
             sender_count[random_province.sender_type] += 1
-            print random_province.province_number, random_province.sender_type
+            #print random_province.province_number, random_province.sender_type
             #print "\n"
 print sender_list
 count = Counter(sender_list)
 print count
 print sender_count
-        
-problem = 0
-# check the adjacent sender types and alert if there is a problem
-for province in provinces:
-    #print province.province_number, " borders", province.borders,"Provinces:", province.adjacent, "has sender type", province.sender_type
-    for province_adjacent in province.adjacent:
-        if (provinces[province_adjacent].sender_type == province.sender_type):
-            print "problem:", province.province_number, " and ", provinces[province_adjacent].province_number, "have the same sender type"
-            problem+=1
+check()        
 
-print "Problems:", problem
+
        
         
