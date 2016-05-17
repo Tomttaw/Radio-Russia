5bb3c2e5d072cc7f9123b733dc550acc96ccba1d
ShannonBakker <shannonc2m@live.nl>
2016-05-17 13:31:25 +0200

    update probleem 1

--- ukrainecode_probleem1.py
+++ ukrainecode_probleem1.py
@@ -16,13 +16,14 @@ class Province(object):
     A Province represents a province of Ukraine
     """
 
-    def __init__(self, province_number, adjacent):
+    def __init__(self, province_number, uneven, adjacent,):
         """
         Initializes a province with a random zender-type
         """
         self.province_number = province_number
+        self.uneven = uneven
         self.adjacent = adjacent
-        self.borders = len(adjacent)
+        self.amount_of_borders = len(adjacent)
         self.sender_type = None     
 
 provinces = []
@@ -33,22 +34,9 @@ def inimap(filename):
         for row in ukrainereader:
             adjacent = []
             for i in range(len(row)-1):
-                #i+=1
                 if (i>1) and (row[i]):
                     adjacent.append(int(row[i]))
-            print adjacent
-            provinces.append(Province(int(row[0]),adjacent))
-
-        
-inimap('oekraine_priority.csv')      
-
-volgorde = [7,5,6,4,3,2,1]
-# boolean toevoegen wordt oneven omringd, wordt even omringd
-
-sender_list = []
-prices = []
-sender_count = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0, "G": 0}
-
+            provinces.append(Province(int(row[0]),int(row[1]),adjacent))
 
 """
 Get list of senders possible for province
@@ -61,9 +49,8 @@ def getpossible(provincelist):
     for adj_province in provincelist:
         if provinces[adj_province].sender_type in sender_lis1:
             sender_lis1.remove(provinces[adj_province].sender_type)
-    if not sender_lis1:
-        print "No sendertype possible"
-        sys.exit()        
+    #if not sender_lis1:
+        #print "No sendertype possible"        
     return sender_lis1  
 
 """
@@ -72,61 +59,58 @@ Distribute four sendertypes evenly over all provinces
 def evendistr(sendercount, possible_senders):
     possible_dict = dict((k, sendercount[k]) for k in possible_senders)
     province_sender = min(possible_dict, key=possible_dict.get)
-    return province_sender      
+    return province_sender
 
-# Check the adjacent sender types and alert if there is a problem
-def check():
-    problem = 0    
-    for province in provinces:
-        # Print province.province_number, " borders", province.borders,"Provinces:", province.adjacent, "has sender type", province.sender_type
-        for province_adjacent in province.adjacent:
-            if (provinces[province_adjacent].sender_type == province.sender_type):
-                print "problem:", province.province_number, " and ", provinces[province_adjacent].province_number, "have the same sender type"
-                problem+=1
-    if (problem > 0):
-        print "Problems:", problem
 
-def inirandom():
-    for province in random.sample(provinces, len(provinces)):
-        #sender_lis2 = ["A", "B", "C", "D", "E", "F", "G"]
-        possible_list = getpossible(province.adjacent)
-        province.sender_type = random.choice(possible_list)
-        
-        #sender_list.append(province.sender_type)
-        #sender_count[province.sender_type] += 1
-def lowest_greed():
-    for province in provinces:
-        #print province.province_number
-        possible_list = getpossible(province.adjacent)
-        province.sender_type = possible_list[0]
-        sender_list.append(province.sender_type)
-        sender_count[province.sender_type] += 1   
-    
 
-for i in volgorde:
-    # make a list of the provinces with the same borders
-    same_borders = []
-    for provi in provinces:
-        if provi.borders == i:
-            same_borders.append(provi)
-    if not same_borders:
-       continue
-    # give the provinces in the same borders list a sender type. 
-    else:
-        while len(same_borders) != 0:
-            #pick a random province from the list
-            random_province = random.choice(same_borders)
-            same_borders.remove(random_province)
+inimap('oekraine_priority.csv')      
+correct_solutions = 0
+false_solutions = 0
 
-            # get the possible sender types
-            possible_senders = getpossible(random_province.adjacent)
+for i in range(1000):
+    sender_list = []
+    prices = []
+    sender_count = {"A": 0, "B": 0, "C": 0, "D": 0}
+    stop_loop = 0
 
-            # distribute the senders evenly
-            random_province.sender_type = evendistr(sender_count, possible_senders)                        
+    for i in range(10,0,-1):
+        # stop when there are no possible solutions
+        if stop_loop == 1:
+                continue
+        # make a list of the provinces with the same amount of borders
+        same_borders = []
+        for province in provinces:
+            if province.amount_of_borders == i:
+                same_borders.append(province)
+        if not same_borders: 
+            continue
+        # give the provinces in the same borders list a sender type. 
+        else:
+            while len(same_borders) != 0:
+                # stop when there are no possible solutions
+                if stop_loop == 1:
+                    break
+                #pick a random province from the list
+                random_province = random.choice(same_borders)
+                same_borders.remove(random_province)
+
+                # get the possible sender types
+                possible_senders = getpossible(random_province.adjacent)
+                if not possible_senders:
+                    false_solutions+=1
+                    stop_loop=1
+                    continue
+                    
+                # distribute the senders evenly
+                random_province.sender_type = evendistr(sender_count, possible_senders)                        
             
-            sender_count[random_province.sender_type] += 1
-            print random_province.province_number, random_province.sender_type
-print sender_count
+                sender_count[random_province.sender_type] += 1
+                #print random_province.province_number, random_province.sender_type
+    #print "end of solution"
+
+
+print false_solutions
+
 
 
         
