c21873f3b244b66c202a06453531d5fec2a044fe
ShannonBakker <shannonc2m@live.nl>
2016-05-19 14:54:12 +0200

    Probleem 1 jipie!!!!!

---
+++ ukrainecode_probleem_solution.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+import csv
+import random
+import sys
+from collections import Counter
+
+class Province(object):
+    """
+    A Province represents a province of Ukraine
+    """
+
+    def __init__(self, province_number, uneven, adjacent,):
+        """
+        Initializes a province with a random zender-type
+        """
+        self.province_number = province_number
+        self.uneven = uneven
+        self.adjacent = adjacent
+        self.amount_of_borders = len(adjacent)
+        self.sender_type = None     
+
+provinces = []
+
+def initialize_map(filename):
+    with open(filename, 'rb') as csvfile:
+        ukrainereader = csv.reader(csvfile, delimiter = ';')
+        for row in ukrainereader:
+            adjacent = []
+            for i in range(len(row)):
+                if (i>1) and (row[i]):
+                    adjacent.append(int(row[i]))
+            provinces.append(Province(int(row[0]),int(row[1]),adjacent))
+
+"""
+Get list of senders possible for province
+"""
+
+def getpossible(provincelist):
+    
+    sender_lis1 = ["A", "B", "C", "D"]
+    # iterate over adjacent provinces and return list of possible sendertypes
+    for adj_province in provincelist:
+        if provinces[adj_province].sender_type in sender_lis1:
+            sender_lis1.remove(provinces[adj_province].sender_type)
+    if not sender_lis1:
+        print "No sendertype possible"
+        sender_lis1 = ["E"]
+    return sender_lis1  
+
+"""
+Distribute four sendertypes evenly over all provinces
+"""
+def evendistribute(sendercount, possible_senders):
+    possible_dict = dict((k, sendercount[k]) for k in possible_senders)
+    province_sender = min(possible_dict, key=possible_dict.get)
+    return province_sender
+
+initialize_map('oekraine_priority.csv')      
+correct_solutions = 0
+false_solutions = 0
+volgorde = [7,5,6,4,3,2,1]
+
+# run the program x times
+for i in range(100):
+    sender_count = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0}
+
+    for j in range(10,0,-1):
+        # make a list of the provinces with the same amount of borders
+        same_borders = []
+        for province in provinces:
+            if province.amount_of_borders == j:
+                same_borders.append(province)
+        # check if there are any provinces in the same_borders list
+        if not same_borders: 
+            continue
+        # give the provinces in the same borders list a sender type. 
+        else:
+            while len(same_borders) != 0:
+
+                #pick a random province from the same borders list
+                random_province = random.choice(same_borders)
+                same_borders.remove(random_province)
+
+                # get the possible sender types
+                possible_senders = getpossible(random_province.adjacent)
+                print possible_senders
+                    
+                # distribute the senders evenly
+                random_province.sender_type = evendistribute(sender_count, possible_senders)                        
+            
+                sender_count[random_province.sender_type] += 1
+                print random_province.province_number, random_province.sender_type
+    for province in provinces:
+        province.sender_type = None
+    if sender_count["E"] > 0:
+        false_solutions += 1
+    print "end of solution"
+    print sender_count
+    
+
+print false_solutions
+
+
+
+        
